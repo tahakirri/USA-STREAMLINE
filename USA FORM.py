c219db3cfe9c0d3ec7b1fad4e149ce208b049843
tahakirri <taha.kirri@gmail.com>
2025-03-26 16:56:37 +0000

    Update USA FORM.py

--- USA FORM.py
+++ USA FORM.py
@@ -611,9 +611,10 @@ else:
             st.session_state.authenticated = False
             st.rerun()
 
-   st.markdown(f"<h1 style='color: var(--primary);'>{nav_items[st.session_state.current_section][0]} {nav_items[st.session_state.current_section][1]}</h1>", unsafe_allow_html=True)
+  st.markdown(
+    f"<h1 style='color: var(--primary);'>{nav_items[st.session_state.current_section][0]} {nav_items[st.session_state.current_section][1]}</h1>",
+    unsafe_allow_html=True)
 
-    # Requests Section
     if st.session_state.current_section == "requests":
         col1, col2 = st.columns([1, 2])
         
